59a4a517600d8f36fd6a86145c2ecede0c02f306
Nazar Kuzmenko <nazar.kuzmenko@gmail.com>
2017-12-10 02:38:42 +0200

    feat(server): create an express server

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import config from 'config'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+import { createLogger } from './modules/logger'
+
+const log = createLogger('server', true)
+const server = express()
+
+// TODO: define cors policy
+server.use(cors())
+server.use(helmet())
+server.use(morgan('tiny', { stream: log.stream }))
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+
+export function start () {
+  return new Promise((resolve, reject) => {
+    server.on('error', reject)
+    server.listen(config.get('server.port'), function onServerStarted () {
+      const { address, port } = this.address()
+
+      log.info('Started')
+      log.info(`host: ${address}`)
+      log.info(`port: ${port}`)
+
+      resolve()
+    })
+  })
+}
+
+export default server
